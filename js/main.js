dc25fb4e07c06524300ada66e4814021d9c06ca0
Edi <40004403+Edi-Gaming@users.noreply.github.com>
2019-07-09 17:22:22 -0400

    yeet

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-	function changeTab(s) {
-		$('.container .row').find('div.tab').each(function(){
-		console.log($( this ).attr("id"));
-			$( this ).fadeOut(100);
-		});
-		$('.container .head-navbar').find('a').each(function(){
-		console.log($( this ).attr("class"));
-			$( this ).removeClass("active");
-		});
-		$("." + s).addClass("active");
-		setTimeout(function(){ $("#" + s).fadeIn(100); }, 100);
-		
-	}
+function changeTab(s) {
+  $(".container .row")
+    .find("div.tab")
+    .each(function() {
+      console.log($(this).attr("id"));
+      $(this).fadeOut(100);
+    });
+  $(".container .head-navbar")
+    .find("a")
+    .each(function() {
+      console.log($(this).attr("class"));
+      $(this).removeClass("active");
+    });
+  $("." + s).addClass("active");
+  setTimeout(function() {
+    $("#" + s).fadeIn(100);
+  }, 100);
+}
